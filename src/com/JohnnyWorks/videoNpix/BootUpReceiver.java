74d1ce40b8bfa5099ab3e3d231e9763cdf718f3d
0x55AA <706044440@qq.com>
2013-12-02 14:10:02 +0800

    Version:2.2 Base on:2.1 修改：UI显示方式

--- src/com/JohnnyWorks/videoNpix/BootUpReceiver.java
+++ src/com/JohnnyWorks/videoNpix/BootUpReceiver.java
@@ -8,20 +8,11 @@ public class BootUpReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		// 轉交給BootupInit處理
-//		ZuniMachineLib.logToText("Receive boot signal, starting MockScr...");
 		Intent i = new Intent(context, MockScreen.class);
 		i.putExtra("DelayStart", true);
 		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		context.startActivity(i);
-		// String packageStr = "com.skype.raider";
-		// String activityStr = "com.skype.raider.Main";
-		//
-		// Intent intent1 = new Intent(Intent.ACTION_MAIN, null);
-		// intent1.addCategory(Intent.CATEGORY_LAUNCHER);
-		// intent1.setComponent(new ComponentName(packageStr, activityStr));
-		// intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		// context.startActivity(intent1);
+
 
 	}
 
